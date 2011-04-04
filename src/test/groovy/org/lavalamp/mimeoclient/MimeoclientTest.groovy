b5c14d3940c7a6d52a7c3098dcfddbac4730eedc
Sean McDaniel <sean.mcdaniel@me.com>
2011-04-04 14:30:54 -0400

    better test coverage

--- src/test/groovy/org/lavalamp/mimeoclient/MimeoclientTest.groovy
+++ src/test/groovy/org/lavalamp/mimeoclient/MimeoclientTest.groovy
@@ -1,6 +1,7 @@
 package org.lavalamp.mimeoclient
 
 import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertEquals
 
 import org.junit.After
 import org.junit.Before
@@ -12,20 +13,12 @@ import redis.clients.jedis.Jedis
 class MimeoclientTest {
   def client
   def jedis
-  def job
    
   @Before
   void setUp() {
     jedis = new Jedis('localhost')
     jedis.flushAll()
     client = new FixtureMimeoclient()
-    job = [
-      'started'       : 'now', 
-      'ended'         : 'now',
-      'text'          : 'text',
-      'status'        : 'success',
-      'num_processed' : '2'
-    ]
   }
 
   @After
@@ -37,7 +30,13 @@ class MimeoclientTest {
   @Test
   void onPMMessage() {
     def t = Thread.start {
-      jedis.hmset 'mimeograph:job:test', job
+      jedis.hmset 'mimeograph:job:test', [
+        'started'       : 'now', 
+        'ended'         : 'now',
+        'text'          : 'text',
+        'status'        : 'success',
+        'num_processed' : '2'
+      ]
       jedis.publish 'mimeograph:job:test', 'mimeograph:job:test:complete'
       Thread.sleep 1000
       client.end()
@@ -50,6 +49,13 @@ class MimeoclientTest {
   static class FixtureMimeoclient extends Mimeoclient {
     def process(job) {
       assertNotNull job
+      job.with {
+        assertEquals 'now', get('started')
+        assertEquals 'now', get('ended')
+        assertEquals 'text', get('text')
+        assertEquals 'success', get('status')
+        assertEquals '2', get('num_processed')
+      }
     }
   }
 }
